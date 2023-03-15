154d0d493a79e7c35e82d5fe223ab0f4e807445b
mukanovaakmaral <126714295+mukanovaakmaral@users.noreply.github.com>
2023-03-15 16:29:55 +0600

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <AlserTabbar :tabs="tab" />
+
+</template>
+
+<script setup lang="ts">
+
+import AlserTabbar from "@/components/AlserTabbar.vue";
+import {ref, Ref} from "vue";
+
+const tab: Ref<any> = ref([
+  {value:'text'},
+  {value:'istext'},
+  {value:'istext'},
+  {value:'istext'}
+])
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="tab-naming">
+    <div v-for="tab in tabs"
+         class="tab"
+         :style="{ width: tabWidth + '%' }"
+         :class="{ activeTab: activeTab === index }"
+         @click="activeTab = index">
+      {{ tab.value }}
+    </div>
+
+    <div class="line-wrapper">
+      <div
+          :style=" { width: avtiveTab + '%', left:(activeTab * this.avtiveTab) + '%'}"
+          class="line"
+      >
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+const props = defineProps({
+  tabs: {
+    type: Array,
+    required: true,
+  }
+})
+
+
+</script>
+
+<style scoped>
+.tab-naming {
+  display: flex;
+  width: 100%;
+  align-items: center;
+}
+
+
+</style>
